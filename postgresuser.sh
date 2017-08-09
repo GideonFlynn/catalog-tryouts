dcbfaf611cfe2efb0066878aaf9ade0c442c045c
Mathias Kandelsdorff <Allmynameswastaken@gmail.com>
2017-08-09 18:50:29 +0200

    Update postgresuser.sh

--- postgresuser.sh
+++ postgresuser.sh
@@ -1,1 +1,14 @@
-su -c "createuser -DRS --password catalog" postgres
+#!/bin/bash
+su -c "createuser -DRS catalog" postgres
+su postgres bash -c "psql -c \"ALTER USER catalog WITH PASSWORD 'catalog';\""
+
+# sudo nano /etc/apache2/sites-available/catalog.conf
+
+
+
+#cd /var/www/flaskapps/catalog
+#sudo pip install virtualenv
+#sudo virtualenv venv
+#source venv/bin/activate
+#sudo pip install -r /var/www/flaskapps/catalog/requirements.txt
+#deactivate
